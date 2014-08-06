9bf4a7891fb6308de95026109a23b8915f9e795a
joomlathememaster <joomlathememaster@gmail.com>
2014-08-06 08:48:38 +0700

    sync8

--- themes/default/register.php
+++ themes/default/register.php
@@ -1,4 +1,4 @@
-	<div id="jmregister" class="jmdefault jmregister jm_login aaa">
+	<div id="jmregister" class="jmdefault jmregister jm_login">
 		<div class="jm-inner"> 
 			<div class="jm-user-images"><span><i class="fa fa-pencil"></i></span></div>
 			<form method="POST" id="form-register" class="form-horizontal" action="<?php echo JRoute::_('index.php', true); ?>" >
